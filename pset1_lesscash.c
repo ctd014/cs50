605709d9ca0a874712fb31c530b14d84c6d12638
ctd014 <35750455+ctd014@users.noreply.github.com>
2018-02-01 16:39:13 -0500

    Create pset1_lesscash.c

---
+++ pset1_lesscash.c
@@ -0,0 +1,31 @@
+//import libraries
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //get change due (e.g. if $9.27 is due, input as 9.27)
+    float c;
+    do
+    {
+    c = get_float("Change owed: ");
+    }
+    while (c<0);
+
+    //convert to cents for simplicity of later arithmetic
+    int cents = round(c*100);
+
+    //determine the appropriate numbers of coins to dispense
+    int quarters = cents/25;
+    int dimes = (cents%25)/10;
+    int nickels = ((cents%25)%10)/5;
+    int pennies = (((cents%25)%10)%5);
+
+    //sum up total number of coins
+    int coins = quarters+dimes+nickels+pennies;
+
+    //print out the number of coins due to customer
+    printf("%d\n", coins);
+
+}
