f7dc63d93ec859611f501b55dfbd61b1282714d5
ctd014 <35750455+ctd014@users.noreply.github.com>
2018-02-01 16:06:25 -0500

    Create pset1_lessmario.c

---
+++ pset1_lessmario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+// prompts user for height
+// only accepts valid heights
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n<0 || n>23);
+
+
+// makes pyramid
+for (int i = 1; i<n+1; i++)
+    {
+    for (int j = 0; j<n-i; j++)
+    {
+        printf(" ");
+    }
+    for (int k = 0; k < i+1; k++)
+    {
+        printf("#");
+    }
+    printf("\n");
+    }
+}
